88878b7a924e63111d91ebf7b32cce7b15c883da
mhuertas <manuel.huertas.lopez@gmail.com>
2018-07-13 06:56:12 +0100

    Clean up

--- src/Encoder.h
+++ src/Encoder.h
@@ -3,27 +3,27 @@
 
 #include "Arduino.h"
 
-#define ENCODER_DEBUG 1
+//#define ENCODER_DEBUG 1
 
 //Maximun number of encoder this class can handle.
 #define MAX_ENCODER 4
 
 class Encoder {
   public:
-		static Encoder *instance_[MAX_ENCODER];
-		static int encoder_index_;
-		Encoder(int ticks_per_revolution);					//constructor with ticks per revolution
-		void attach(int pin);								//attach the encoder interrupt pin
-		void handler(int index);  							//handle the interruption
-		void setupDirection(int direction)					//encoder direction positive fordware negative backware
-			{ this->direction_=direction;};					
-		float getVelocity(float dt);						//velocity [radians per second]
-		inline long getTicks() { return ticks_;}	//total number of ticks 
+	static Encoder *instance_[MAX_ENCODER];
+	static int encoder_index_;
+	Encoder(int ticks_per_revolution);			//constructor with ticks per revolution
+	void attach(int pin);						//attach the encoder interrupt pin
+	void handler(int index);  					//handle the interruption
+	void setupDirection(int direction)			//encoder direction positive fordware negative backware
+		{ this->direction_=direction;};					
+	float getVelocity(float dt);				//velocity [radians per second]
+	inline long getTicks() { return ticks_;}	//total number of ticks 
   private:
-  	int pin_;												//interrupt pin
-  	long ticks_;									//total number of ticks
-	long previous_ticks_;							//store previous ticks since getVelocity was called 
-	float angle_per_tic_;									//factor to convert from ticks to angle.
-	int direction_;											//encoder direction positive fordware negative backware	
+  	int pin_;									//interrupt pin
+  	long ticks_;								//total number of ticks
+	long previous_ticks_;						//store previous ticks since getVelocity was called 
+	float angle_per_tic_;						//factor to convert from ticks to angle.
+	int direction_;								//encoder direction positive fordware negative backware	
 };
 #endif
