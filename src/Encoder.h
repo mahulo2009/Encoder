7f01802ebb83977305b2d4ae82a4a1daef56c2a5
mhuertas <manuel.huertas.lopez@gmail.com>
2018-11-19 06:23:36 +0000

    Refactoring to use a factory class to create robot

--- src/Encoder.h
+++ src/Encoder.h
@@ -1,24 +1,31 @@
 #ifndef Encoder_H
 #define Encoder_H
 
+
 #include "Arduino.h"
+#include "EncoderBase.h"
 
 //#define ENCODER_DEBUG 1
 
 //Maximun number of encoder this class can handle.
 #define MAX_ENCODER 4
 
-class Encoder {
+class Encoder : public EncoderBase {
   public:
 	static Encoder *instance_[MAX_ENCODER];
 	static int encoder_index_;
+
 	Encoder(int ticks_per_revolution);			//constructor with ticks per revolution
+
+	virtual void setupDirection(int direction)			//encoder direction positive fordware negative backware
+		{ this->direction_=direction;};				
+
+	virtual float getVelocity(float dt);				//velocity [radians per second]
+
 	void attach(int pin);						//attach the encoder interrupt pin
 	void handler(int index);  					//handle the interruption
-	void setupDirection(int direction)			//encoder direction positive fordware negative backware
-		{ this->direction_=direction;};					
-	float getVelocity(float dt);				//velocity [radians per second]
 	inline long getTicks() { return ticks_;}	//total number of ticks 
+
   private:
   	int pin_;									//interrupt pin
   	long ticks_;								//total number of ticks
